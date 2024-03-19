461c63b20c47c6a6a241bdd4da3c9bb9b8a6d892
M.Manwani <mmanwani@uoguelph.ca>
2024-03-19 15:59:59 -0400

    bird flapping

--- flappy_bird.py
+++ flappy_bird.py
@@ -23,9 +23,28 @@ ground_img = pygame.image.load('img/ground.png')
 class Bird(pygame.sprite.pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('img/bird1.png')
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for num in range(1, 4):
+            img = pygame.image.load(f'img/bird{num}.png')
+            self.images.append(img)
+        self.image = self.images[self.index]
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
+        
+    def update(self):
+        
+        #handle the animation
+        self.counter += 1
+        flap_cooldown = 5
+        
+        if self.counter > flap_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images):
+                self.index = 0
+        self.image = self.images[self.index]
         
         
 bird_group = pygame.sprite.Group()
@@ -44,6 +63,7 @@ while run:
     screen.blit(bg, (0, 0))
     
     bird_group.draw(screen)
+    bird_group.update()
     
     #draw and scroll the ground
     screen.blit(ground_img, (ground_scroll, 768))
